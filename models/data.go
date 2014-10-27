35836ddc0ed4d68787c652d30d1827f94842dff8
Ikenna N. Okpala <me@ikennaokpala.com>
2014-10-27 22:08:33 +0000

    Add json go tags

--- models/data.go
+++ models/data.go
@@ -1,12 +1,12 @@
 package model
 
 type Site struct {
-	Base Blog
+	Base Blog `json:"blog"`
 }
 
 type Blog struct {
-	Post   Posts
-	Medium Media
+	Post   Posts `json:"post"`
+	Medium Media `json:"medium"`
 }
 
 type Posts map[string]int
