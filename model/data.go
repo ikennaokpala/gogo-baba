a08d21f040b091934087ccebcb5a25b0c587a899
Ikenna Okpala <me@ikennaokpala.com>
2014-11-21 15:48:08 +0000

    Conection to Mysql

--- model/data.go
+++ model/data.go
@@ -1,11 +1,13 @@
 package model
 
 import (
+	"database/sql"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql" // This is a blank import
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -28,8 +30,8 @@ type Posts map[string]int
 //Media holds a collection of photos, videos and document data
 type Media map[string]int
 
-// Db represents information about the database
-type Db struct {
+// DbConfig represents information about the database
+type DbConfig struct {
 	Adapter  string
 	Database string
 	Host     string
@@ -37,12 +39,27 @@ type Db struct {
 	Password string
 }
 
+// Drive the connection parameters
+// from the DbConfig properties
+func (db DbConfig) ConnectString() string {
+	// This is the connection String
+	// meant for connecting to the
+	connectString := db.User + ":" + db.Password + "@/" + db.Database
+	return connectString
+}
+
+// Connect to Database
+func (db DbConfig) Connect() *sql.DB {
+	con, _ := sql.Open(db.Adapter, db.ConnectString())
+	return con
+}
+
 // ReadJSON read and returns content of
 // a yaml file passed to it
-func ReadJSON() Db {
+func ReadJSON() DbConfig {
 
-	db := Db{}
-	path := DbPath("json")
+	db := DbConfig{}
+	path := DbConfigPath("json")
 
 	data, _ := ioutil.ReadFile(path)
 	err := json.Unmarshal([]byte(data), &db)
@@ -50,16 +67,15 @@ func ReadJSON() Db {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	return db
 }
 
 // ReadYAML read and returns content of
 // a yaml file passed to it
-func ReadYAML() Db {
+func ReadYAML() DbConfig {
 
-	db := Db{}
-	path := DbPath("yml")
+	db := DbConfig{}
+	path := DbConfigPath("yml")
 
 	data, _ := ioutil.ReadFile(path)
 	err := yaml.Unmarshal([]byte(data), &db)
@@ -71,15 +87,11 @@ func ReadYAML() Db {
 	return db
 }
 
-// DbPath returns database.yml path
-func DbPath(ext string) string {
+// DbConfigPath returns database.yml path
+func DbConfigPath(ext string) string {
 
 	path, _ := os.Getwd()
 	dbPath := path + "/config/database." + ext
 
 	return dbPath
-}
-
-func connect() {
-
 }
