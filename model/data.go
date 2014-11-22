926813b53b648d6e81296dbf5fac244711b2fbc2
Ikenna Okpala <me@ikennaokpala.com>
2014-11-22 01:18:01 +0000

    Rename DbConfig back to Db

--- model/data.go
+++ model/data.go
@@ -30,8 +30,8 @@ type Posts map[string]int
 //Media holds a collection of photos, videos and document data
 type Media map[string]int
 
-// DbConfig represents information about the database
-type DbConfig struct {
+// Db represents information about the database
+type Db struct {
 	Adapter  string
 	Database string
 	Host     string
@@ -40,8 +40,8 @@ type DbConfig struct {
 }
 
 // Drive the connection parameters
-// from the DbConfig properties
-func (db DbConfig) ConnectString() string {
+// from the Db properties
+func (db Db) ConnectString() string {
 	// This is the connection String
 	// meant for connecting to the
 	connectString := db.User + ":" + db.Password + "@/" + db.Database
@@ -49,7 +49,7 @@ func (db DbConfig) ConnectString() string {
 }
 
 // Connect to Database
-func (db DbConfig) Connect() *sql.DB {
+func (db Db) Connect() *sql.DB {
 	con, err := sql.Open(db.Adapter, db.ConnectString())
 
 	if err != nil {
@@ -59,12 +59,16 @@ func (db DbConfig) Connect() *sql.DB {
 	return con
 }
 
+func (db Db) Read() {
+
+}
+
 // Reads onfiguration YAML or JSON  and returns content of
 // the coreesponding file passed to it
-func ReadConfigFile(ext string) DbConfig {
+func ReadConfigFile(ext string) Db {
 	var err error
 
-	db := DbConfig{}
+	db := Db{}
 	path := ConfigPath(ext)
 
 	data, _ := ioutil.ReadFile(path)
